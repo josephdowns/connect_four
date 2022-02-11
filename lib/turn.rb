4f112b083ee9a4a7070abbef3d07a8d4ef35e2a4
Kim G <xiaole.guo@yahoo.com>
2022-02-11 13:43:00 -0700

    moved turn-invalid from runner to Turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,15 +11,22 @@ class Turn
       column = column_selected.upcase
       current_column = {}
       @current_board = current_board #initialize current board with empty
+      column_name = ["A", "B", "C", "D", "E", "F","G" ]
 
+      if !column_name.include?column
+         # puts "ABCDEFG"
+         Board.new(current_board).print_board
+         puts "Invalid column selected. Please try again (A-G)"
+         # $stdin.gets.chomp
+      elsif column_name.include?column
 # start with empty board update and print board after each move
 
-        #this block finds and return the user selected column
-        @current_board.each do |key, value|
-          if key.to_s.include?column
-            current_column[key] = value
+       #this block finds and return the user selected column
+          @current_board.each do |key, value|
+            if key.to_s.include?column
+              current_column[key] = value
+            end
           end
-        end
 
           # in the selected column, only replaces a . when there is still one available
           if current_column.values.include?'.'
@@ -33,15 +40,23 @@ class Turn
           else
             # if no cell available prompt to select again
             Board.new(@current_board).print_board
-            puts "Column full-select another column"
+            puts "***Column full-select another column***"
           end
-  end
-  def npc_move(current_board)
+
+      end # if.. elsif ..line 16 and 21
+  end #player_move end
+
+
+  def computer_move(current_board)
+
+
+
 
   end
 
 
-  def end_game?
+  def end_game?(current_board)
+    @current_board = current_board
 
 
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,40 +8,66 @@ turn = Turn.new
 
 RSpec.describe Turn do
 
-  it "test the move" do
+  context "player_move" do
+      it "test the move" do
+        expect{turn.player_move('G', empty_board)}.to output(
+          <<~EXPECTED
+          .......
+          .......
+          .......
+          .......
+          .......
+          ......x
+           EXPECTED
+          ).to_stdout
+      end
 
-    expect{turn.player_move('G', empty_board)}.to output(
-      <<~EXPECTED
-      .......
-      .......
-      .......
-      .......
-      .......
-      ......x
-       EXPECTED
-      ).to_stdout
+      it "is an invalid move" do
+        turn_new = Turn.new
+        eb= Board.new({}).empty_board
+          expect{turn_new.player_move("Z", eb)}.to output(
+            <<~EXPECTED
+            .......
+            .......
+            .......
+            .......
+            .......
+            .......
+            Invalid column selected. Please try again (A-G)
+              EXPECTED
+          ).to_stdout
+        end
+
+      it "tests when column is full" do
+        current_board = empty_board
+        6.times do
+          turn.player_move('G', current_board)
+        end
+        expect{turn.player_move('G', empty_board)}.to output(
+          <<~EXPECTED
+          ......x
+          ......x
+          ......x
+          ......x
+          ......x
+          ......x
+          ***Column full-select another column***
+           EXPECTED
+         ).to_stdout
+      end
+
+
   end
 
-  it "test when column is full" do
 
-    current_board = empty_board
-    6.times do
-      turn.player_move('G', current_board)
+  context "computer_move" do
+    xit "tests whether two imputs give different outputs" do
+      turn_new = Turn.new
+      eb= Board.new({}).empty_board
+      expect{turn_new.computer_move(eb)}.to !eq(turn_new.computer_move(eb))
     end
-    expect{turn.player_move('G', empty_board)}.to output(
-      <<~EXPECTED
-      
-      ......x
-      ......x
-      ......x
-      ......x
-      ......x
-      ......x
-      ---Column full-select another column---
-       EXPECTED
-      ).to_stdout
-  end
 
+  end
 
 
 
