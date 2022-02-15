c49beb1460e98f2568fcf25b9a53bebd7290d800
Kim G <xiaole.guo@yahoo.com>
2022-02-15 11:39:00 -0700

    refactor runner and game file

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,31 @@
+require './lib/board'
+require './lib/turn'
+
 class Game
 
+  def main_menu
+    puts ""
+    puts "-"*8 + "Main Menu" + "-"*8
+    puts "Welcome to Connect Four!"
+    puts "Enter 'p' to play. Enter 'q' to quit."
+    puts ""
+    print "> "
+    menu_selection = $stdin.gets.chomp.upcase.strip
+
+    until menu_selection == "P" || menu_selection == "Q" do
+      puts "Invalid selection. Try again!"
+      puts ""
+      print "> "
+      menu_selection = $stdin.gets.chomp.upcase.strip
+    end
+
+    if menu_selection == "P"
+       Game.new.start
+    elsif menu_selection == "Q"
+      puts ""
+      puts "Maybe next time! Bye!"
+    end
+  end
 
   def start
     puts ""
@@ -13,42 +39,38 @@ class Game
     print "> "
     current_board = empty_board
     column_selected = $stdin.gets.chomp.strip
-    # valid = turn_init.valid_move?(column_selected)
-
-    until turn_init.end_game?(current_board) == true do
+    initial_column = column_selected.dup
+    initial_cell_key = ":".concat(initial_column.concat("1"))
+    until turn_init.end_game_draw?(current_board) == true do
 
       valid = turn_init.valid_move?(column_selected)
-
-              # turn_init.player_move(new_column_selected, current_board)
-              # binding.pry
-
-          until valid == true do
-            column_selected = $stdin.gets.chomp.strip
-            # Board.new(current_board).print_board
-            valid = turn_init.valid_move?(column_selected)
-            # binding.pry
-          end
-          turn_init.player_move(column_selected, current_board)
-          current_board = turn_init.current_board
-          if turn_init.end_game?(current_board) == true
-              puts ""
-              break
-          end
-
+      until valid == true do
+        column_selected = $stdin.gets.chomp.strip
+        valid = turn_init.valid_move?(column_selected)
+      end
+      turn_init.player_move(column_selected, current_board)
+      current_board = turn_init.current_board
+      current_cell = turn_init.current_cell[0].to_s
+      if turn_init.end_game?(current_cell, current_board) == true
           puts ""
-          puts "*"*5 + "My turn! Give me a sec..." + "*"*5
-          cpu_move = turn_init.computer_move(current_board)
-          # puts "I chose #{cpu_move.current_cell_index}!"
-          current_board = turn_init.current_board
-          if turn_init.end_game?(current_board) == true
-              puts ""
-              break
-          end
+          break
+      end
+      puts ""
+      puts "*"*5 + "My turn! Give me a sec..." + "*"*5
+      cpu_move = turn_init.computer_move(current_board)
+      current_board = turn_init.current_board
+      current_cell = turn_init.current_cell[0].to_s
+
+      if turn_init.end_game?(current_cell, current_board) == true
           puts ""
-          puts "*"*5 + "Your turn! Select a column" + "*"*5
-          puts ""
-          print "> "
-          column_selected = $stdin.gets.chomp.strip
+          break
+      end
+      puts ""
+      puts "*"*5 + "Your turn! Select a column" + "*"*5
+      puts ""
+      print "> "
+      column_selected = $stdin.gets.chomp.strip
     end
+    main_menu
   end
 end
